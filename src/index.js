712b1ec1893904fe3c7234e8740daab1022ec2d5
unknown <debadyuti.karmakar@gmail.com>
2022-03-20 20:32:37 +0530

    Tictacto

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import React from 'react';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Board extends React.Component{
+    render(){
+        return(
+            <div className='board'>Game Board</div>
+        );
+    }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Display extends React.Component{
+    render(){
+        return(
+            <div className='display'>Game Display</div>
+        );
+    }
+}
+class TTT extends React.Component{
+
+    render(){
+        let name = "Debadyuti";
+
+        return (
+            <>
+                <Board />
+                <Display />
+            </>
+        );
+    }
+}
+ReactDOM.render(<TTT />, document.getElementById("root"));
+

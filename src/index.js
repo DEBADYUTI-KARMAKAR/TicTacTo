80c9fec55b5c78f5cddeaa110e722268a40d7968
unknown <debadyuti.karmakar@gmail.com>
2022-03-20 20:47:58 +0530

    Tictacto

--- src/index.js
+++ src/index.js
@@ -2,26 +2,59 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import './index.css';
 
-class Board extends React.Component{
-    render(){
-        return(
-            <div className='board'>Game Board</div>
+class Board extends React.Component {
+    render() {
+        return (
+            <div className='board'>
+                <div className='title'>
+                    Tic Tac Toe
+                </div>
+                <div className='content'>
+                    <div className="ttt">
+                        <div className='row'>
+                            <button></button>
+                            <button></button>
+                            <button></button>
+                        </div>
+                        <div className='row'>
+                            <button></button>
+                            <button></button>
+                            <button></button>
+                        </div>
+                        <div className='row'>
+                            <button></button>
+                            <button></button>
+                            <button></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
 
-class Display extends React.Component{
-    render(){
-        return(
-            <div className='display'>Game Display</div>
+class Display extends React.Component {
+    render() {
+        let gameStatus = 'Next move for "X"';
+
+        return (
+            <div className='display'>
+                <div className='title'>
+                    {gameStatus}
+                </div>
+                <div className='content'>
+                    <div className='history'>
+                        <button>Go to start</button>
+
+                    </div>
+                </div>
+            </div>
         );
     }
 }
-class TTT extends React.Component{
 
-    render(){
-        let name = "Debadyuti";
-
+class TTT extends React.Component {
+    render() {
         return (
             <>
                 <Board />
@@ -30,5 +63,5 @@ class TTT extends React.Component{
         );
     }
 }
-ReactDOM.render(<TTT />, document.getElementById("root"));
 
+ReactDOM.render(<TTT />, document.getElementById("root"))
